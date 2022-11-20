2b14ab6f1d83cfc0fe30bdbe259395164a8ad4bc
anthonynguyen-dev <nguyen.dev@protonmail.com>
2022-11-20 02:50:38 -0600

    added css in style.css and added temp and humidity in js

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 var apiKey = "9f2dbba4f257d4eb625124918b06faf4";
 var citySearch = document.querySelector("#search-form");
 var searchInput = document.querySelector("#search-input");
-// function getAPI() {
-//     api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-// }
+var windSpd = document.querySelectorAll(".wind-spd");
+var temp = document.querySelectorAll(".temp");
+var humidity = document.querySelectorAll(".humidity");
+var weatherMain = document.querySelectorAll(".weather");
+var nameCity = document.querySelector("#name-city");
+var weatherJSON;
+var date = document.querySelectorAll(".date");
+const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
-function getCurrentWeather(cityName) {
-  var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+citySearch.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var cityName = searchInput.value.trim();
+  getLocation(cityName);
+  console.log(weatherJSON);
+
+  //console.log(weatherJSON.main.temp);
+  //parseJsonData(weatherJSON);
+});
+
+function getCurrentWeather(location) {
+  //var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  console.log(location);
+  var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${location.lat}&lon=${location.lon}&appid=${apiKey}`;
   fetch(weatherURL)
     .then(function (response) {
       if (response.ok) {
@@ -14,13 +31,70 @@ function getCurrentWeather(cityName) {
       }
     })
     .then(function (data) {
+      console.log("data returned is...");
       console.log(data);
+      visualDisplay(data.list);
+      weatherJSON = JSON.stringify(data);
+      console.log(weatherJSON);
     });
-  console.log(cityName);
 }
 
-citySearch.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var cityName = searchInput.value.trim();
-  getCurrentWeather(cityName);
-});
+function getLocation(city) {
+  var locationApi = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+
+  fetch(locationApi)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      getCurrentWeather(data[0]);
+    });
+}
+
+function displayJsonData(jsonData) {
+  console.log("parsing json data");
+  console.log("Displaying Temp.");
+  console.log(jsonData.main.temp);
+  console.log(jsonData.main.humidity);
+  console.log(jsonData.wind.speed);
+}
+
+function getDate() {}
+
+function visualDisplay(forecastDays) {
+  console.log(forecastDays);
+
+  for (var i = 0; i < 40; i += 8) {
+    date.textContent = weatherMain[0].textContent =
+      forecastDays[0].weather.main;
+    weatherMain[1].textContent = forecastDays[8].weather.main;
+    weatherMain[2].textContent = forecastDays[16].weather.main;
+    weatherMain[3].textContent = forecastDays[24].weather.main;
+    weatherMain[4].textContent = forecastDays[32].weather.main;
+
+    temp[0].textContent = forecastDays[0].main.temp + " C";
+    temp[1].textContent = forecastDays[8].main.temp + " C";
+    temp[2].textContent = forecastDays[16].main.temp + " C";
+    temp[3].textContent = forecastDays[24].main.temp + " C";
+    temp[4].textContent = forecastDays[32].main.temp + " C";
+
+    windSpd[0].textContent = forecastDays[0].wind.speed + " mph";
+    windSpd[1].textContent = forecastDays[8].wind.speed + " mph";
+    windSpd[2].textContent = forecastDays[16].wind.speed + " mph";
+    windSpd[3].textContent = forecastDays[24].wind.speed + " mph";
+    windSpd[4].textContent = forecastDays[32].wind.speed + " mph";
+
+    humidity[0].textContent = forecastDays[0].main.humidity + " %";
+    humidity[1].textContent = forecastDays[8].main.humidity + " %";
+    humidity[2].textContent = forecastDays[16].main.humidity + " %";
+    humidity[3].textContent = forecastDays[24].main.humidity + " %";
+    humidity[4].textContent = forecastDays[32].main.humidity + " %";
+
+    date[0].textContent = weekday[0];
+    date[1].textContent = weekday[1];
+    date[2].textContent = weekday[2];
+    date[3].textContent = weekday[3];
+    date[4].textContent = weekday[4];
+  }
+}
